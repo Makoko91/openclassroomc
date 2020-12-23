2c7f9465fa7fe8a9ef12ebb44b68a8bc92bde9f6
Makoko91 <croisille.maxime@gmail.com>
2020-12-23 22:20:33 +0100

    fct melange mot

--- mystere.cpp
+++ mystere.cpp
@@ -5,24 +5,42 @@
 using namespace std;
 
 
-void saisiemot()
+void saisiemot(string &mot)
 {
-    string mot;
+    
     cout << "choissisez un mot"<<endl;
     cin >> mot;
-    srand(time(0));
-    int position = rand()%mot.size();
-    cout << position+1 <<endl;
-    cout << "Lettre au hasard :" << mot[position];
+    
 
 }
-
+void melangelettre(string &mot)
+{
+    string nouveaumot;
+    while (mot.size()!=0)
+    {
+        int position1 = rand()%mot.size();
+        cout << "choix de la " << position1+1 <<" lettre"<<endl;
+        nouveaumot += mot[position1];
+        mot.erase(position1,1);
+    }
+    cout << nouveaumot <<endl;
+/*     cout << "choix de la " << position2+1 <<" lettre"<<endl;
+    cout << "Lettres choisie au hasard :" << mot[position1]<< mot[position2]<<endl; */
+}
 
 int main()
 {    
+    srand(time(0));
     //1 : On demande de saisir un mot
-    saisiemot();
+    string motuser;
+    cout << "debut fct1"<<endl;
+    saisiemot(motuser);
+    cout << "fin fct1"<<endl;
+    cout <<"mot choisie par l'utilisateur "<< motuser<< endl;;
     //2 : On mélange les lettres du mot
+    cout << "debut fct2"<<endl;
+    melangelettre(motuser);
+    cout << "fin fct2"<<endl;
 
     //3 : On demande à l'utilisateur quel est le mot mystère
 
